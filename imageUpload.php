4fa0ecca6c953911bf8d609c9f51ebb5facbf0f4
Kieran Brown <kieranjbrown1@gmail.com>
2018-02-03 23:06:42 +0000

    .

--- imageUpload.php
+++ imageUpload.php
@@ -11,7 +11,7 @@ if(isset($_POST['submit'])) {
     $url = "www.kieranbrown.me/facebook/uploads/";
     $error = 0;
     //print_r($FILES['imageToUpload']);
-    $upload_dir = getcwd() . "\uploads\";
+    $upload_dir = getcwd() . "\\uploads\\";
     $fileName = $_FILES['imageToUpload']['name'];
     $uploaded_file = $upload_dir . $fileName;
     $pythonDirectory = "/uploads/".$fileName;
