d15ce41c2608ddec97e2616055402f6a73721a47
Christopher <cdcyang@gmail.com>
2018-02-03 20:40:46 +0000

    Python file clean up

--- photo_json_generator.py
+++ photo_json_generator.py
@@ -50,3 +50,39 @@ class PhotoJson(object):
             image_data = image_file.read()
             encoded_data = base64.b64encode(image_data)
         return encoded_data.decode('ascii')
+
+
+#function that uses url to get tags
+"""def generate_requestJSON(img_url):
+    request_object = {}
+    request_object["requests"] = []
+    requests = request_object["requests"]
+    requests.append({})
+    first_request = requests[0]
+    first_request["image"] = {"source": {"imageUri": img_url}}
+    first_request["features"] = []
+    first_request["features"].append({"type": "LABEL_DETECTION"})
+    first_request["features"].append({"type": "FACE_DETECTION"})
+    first_request["features"].append({"type": "LANDMARK_DETECTION"})
+    first_request["features"].append({"type": "TEXT_DETECTION"})
+    first_request["features"].append({"type": "LOGO_DETECTION"})
+    return request_object
+
+def getLabelsWithCurl(img_url):
+    start = time.time()
+    request_json = generate_requestJSON_localImg(img_url)
+    with open('request.json', 'w') as request_file:
+        request_file.write(json.dumps(request_json))
+    API_key = "AIzaSyD_olILEcP-FsTGbsu669oLMxHt4wo4QyI"
+    curl_command = "curl -v -s -H \"Content-Type: application/json\" https://vision.googleapis.com/v1/images:annotate?key="
+    curl_command += API_key
+    curl_command += " --data-binary @request.json" 
+    raw_res = os.popen(curl_command).read()
+    res = json.loads(raw_res)
+    end = time.time()
+    print("The time taken to fetch labels is: ", end - start)
+    with open('data.json', 'a') as cache_file:
+        for c in raw_res:
+            if c not in " \n":
+                cache_file.write(c)
+    return res"""
