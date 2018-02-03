e42dd281b8decfa941baf9f6632ea26b153c6155
Kieran Brown <kieranjbrown1@gmail.com>
2018-02-03 20:53:41 +0000

    quick update

--- imageUpload.php
+++ imageUpload.php
@@ -1,9 +1,10 @@
 <?php
-if(!empty($_FILES)){
+if(!isset($_FILES)){
+    $error = 0;
     $upload_dir = "uploads/";
-    $fileName = $_FILES['file']['name'];
+    $fileName = $_FILES['imageToUpload']['name'];
     $uploaded_file = $upload_dir.$fileName;
-    if(move_uploaded_file($_FILES['file']['tmp_name'],$uploaded_file)){
+    if(move_uploaded_file($_FILES['imageToUpload']['tmp_name'],$uploaded_file)){
 //insert file information into db table
         //$mysql_insert = "INSERT INTO uploads (file_name, upload_time)VALUES('".$fileName."','".date("Y-m-d H:i:s")."')";
         //mysqli_query($conn, $mysql_insert) or die("database error:". mysqli_error($conn));
